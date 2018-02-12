90ff63e5744c57d9b054a559c3fea50153810a31
Gdoong426 <dungdudu426@gmail.com>
2018-02-12 21:13:01 +0800

    Add function of restoring trained model and model predition.

--- hw1.py
+++ hw1.py
@@ -18,6 +18,8 @@ import load_policy
 
 
 their_data_path = None
+save_path = None
+save_name = None
 
 
 def view_expert(policy, data_path):
@@ -57,9 +59,6 @@ def view_expert(policy, data_path):
                     break
             returns.append(totalr)
 
-            print(observations)
-            print(actions)
-
         expert_data = {'observations': np.array(observations),
                        'actions': np.array(actions),
                        'returns': np.array(returns)}
@@ -85,9 +84,9 @@ def simple_model(data):
     _actions = np.reshape(actions, (actions.shape[0], actions.shape[2]))
 
     # params
-    learning_rate = 0.025
-    train_iter = 10
-    batch_size = 100  # how many datas contained in one batch
+    learning_rate = 0.001
+    train_iter = 20
+    batch_size = 1000  # how many datas contained in one batch
     num_batches = int(observations.shape[0] / batch_size)
     dropout_rate = 0.9
 
@@ -96,23 +95,38 @@ def simple_model(data):
     output_data = tf.placeholder(tf.float32, shape=[None, _actions.shape[1]])
 
     # fully connected NN
-    hidden = fully_connected(input_data, 3, activation_fn=tf.nn.relu)
-    prediction = fully_connected(hidden, _actions.shape[1], activation_fn=None)
+
+    with tf.variable_scope('NN'):
+        hidden = fully_connected(input_data, 10, activation_fn=tf.nn.relu, biases_initializer=tf.random_normal_initializer)
+        prediction = fully_connected(hidden, _actions.shape[1], activation_fn=None)
+
+    # convert numpy array to tensor
+    observations = tf.cast(observations, tf.float32)
+    _actions = tf.cast(_actions, tf.float32)
 
     # create input pipe line
     input_que = tf.train.slice_input_producer([observations, _actions], shuffle=False)
     batch_input = tf.train.batch(input_que, batch_size=batch_size)
 
-    cost = tf.reduce_mean(tf.square(tf.subtract(output_data, prediction)))
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+    with tf.name_scope('Cost'):
+        cost = tf.reduce_mean(tf.square(tf.subtract(output_data, prediction)), name='cost')
+
+    with tf.name_scope('Train'):
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
     coord = tf.train.Coordinator()
 
+    log_path = 'Tensorboard/'
+    writer = tf.summary.FileWriter(log_path, graph=tf.get_default_graph())
+
     saver = tf.train.Saver()
+
     with tf.Session() as sess:
 
         sess.run(tf.global_variables_initializer())
         print('Initialized global variable...')
-        threads = tf.train.start_queue_runners(coord=coord)
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+        writer = tf.summary.FileWriter(log_path, graph=sess.graph)
 
         for train_step in range(train_iter):
 
@@ -124,34 +138,83 @@ def simple_model(data):
                 print('batch number', i, "/", num_batches)
 
                 _, c = sess.run([optimizer, cost], feed_dict={input_data: batch_observ, output_data: batch_act})
+                # writer.add_summary(c, train_step * num_batches + i)
 
                 avg_cost += c
             print('Average cost:', avg_cost)
             print('-----------------------------')
 
+        global save_path, save_name
+        save_path = 'hw1_model/'
+        save_name = 'hw1_model.ckpt'
+        saver.save(sess, 'hw1_model/hw1_model.ckpt')
+        print('Model saved in path: %s' % (save_path + save_name))
+
         coord.request_stop()
         coord.join(threads)
-        global save_path
-        save_path = saver.save(sess, '/hw1_model/hw_1_model.ckpt')
-        print('Model saved in path: %s' % save_path)
+        return save_path, save_name
 
 
 def train_my_model():
 
     global their_data_path
     expert_data = pickle.load(open(their_data_path, 'rb'))
-    print('Expert model:')
-    print(expert_data)
-
-    x1 = np.array(expert_data['observations'])
-    x2 = np.array(expert_data['actions'])
-
-    print('observations:')
-    print(x1.shape)
-    print('actions')
-    print(x2.shape)
 
     simple_model(expert_data)
+
+
+def restore_model():
+    global save_path, save_name
+
+    meta_path = None
+    if not save_path:
+        meta_path = 'hw1_model/hw1_model.ckpt.meta'
+        meta_folder = 'hw1_model'
+    else:
+        meta_path = save_path + save_name + '.meta'
+        meta_folder = save_path
+    tf.reset_default_graph()
+
+    input_data = tf.placeholder(tf.float32, shape=[None, 11])
+
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(meta_path)
+        saver.restore(sess, tf.train.latest_checkpoint(meta_folder))
+
+        sess.run(tf.global_variables_initializer())
+        all_var = tf.trainable_variables()
+
+        env = gym.make(args.envname)
+        max_steps = args.max_timesteps or env.spec.timestep_limit
+
+        returns = []
+        observations = []
+        actions = []
+
+        for i in range(args.num_rollouts):
+
+            obs = env.reset()
+            done = False
+            steps = 0
+            totalr = 0
+
+            while not done:
+                action = tf_util.eval(all_var[0], feed_dict={input_data: obs[None, :]})
+
+                print(action)
+
+                observations.append(obs)
+                actions.append(action)
+                obs, r, done, _ = env.step(action)
+                totalr += r
+                steps += 1
+                if args.render:
+                    env.render()
+                if steps % 100 == 0:
+                    print("%i/%i" % (steps, max_steps))
+                if steps >= max_steps:
+                    break
+            returns.append(totalr)
 
 
 if __name__ == '__main__':
@@ -174,5 +237,7 @@ if __name__ == '__main__':
     their_data_path = args.envname + "-expert.txt"
 
     #view_expert(policy_fn, their_data_path)
-    train_my_model()
+    #train_my_model()
+
+    restore_model()
     exit()
